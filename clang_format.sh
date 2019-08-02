3f6c6bcbf9787764b79463b048ebcb6782bd34e1
lchannng <l.channng@gmail.com>
2019-08-02 19:57:46 +0800

    toml parsing.

--- clang_format.sh
+++ clang_format.sh
@@ -3,10 +3,7 @@
 find ijk tests \
     \( -name '*.cc' -o -name '*.cpp' -o -name '*.hpp' -o -name '*.ipp' -o -name '*.c' -o -name '*.h' \) \
     -not \( \
-        -path 'ijk/base/details/fmt/*' \
-        -o -path 'ijk/base/details/gsl/*' \
-        -o -path 'ijk/base/details/nonstd/*' \
-        -o -path 'ijk/base/details/termcolor*' \
+        -path 'ijk/base/details/*' \
         -o -path 'ijk/io/asio/*' \
         -o -path 'samples/shadowsocks-ijk/common/json11.*' \
         -o -path 'extras/redis/3rd/*' \
---
+++ ijk/base/cpptoml.hpp
@@ -0,0 +1,13 @@
+/*
+ * File  : cpptoml.hpp
+ * Author: Lch <l.channng@gmail.com>
+ * Date  : 2019/08/02 19:54:29
+ */
+
+#pragma once
+#ifndef CPPTOML_HPP_3IO68MDJ
+#define CPPTOML_HPP_3IO68MDJ
+
+#include "details/cpptoml.h"
+
+#endif /* end of include guard: CPPTOML_HPP_3IO68MDJ */
