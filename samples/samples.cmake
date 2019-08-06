58668d2a71e87aeb5d2b581fd07947322da988c5
lchannng <l.channng@gmail.com>
2019-08-06 20:01:46 +0800

    update.

--- samples/samples.cmake
+++
@@ -1,7 +0,0 @@
-set(SAMPLES_ROOT ${CMAKE_CURRENT_SOURCE_DIR}/samples)
-
-add_executable(sample01 ${SAMPLES_ROOT}/sample01.cc)
-target_link_libraries(sample01 ijk)
-
-add_executable(sample02 ${SAMPLES_ROOT}/sample02.cc)
-target_link_libraries(sample02 ijk)
