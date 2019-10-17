8820b6f9f46a37422bace8f0672fd9eb0304e0eb
lchannng <l.channng@gmail.com>
2019-10-17 16:52:51 +0800

    update.

--- extras/network/samples/main.cc
+++ extras/network/samples/main.cc
@@ -9,16 +9,21 @@
 int main(int argc, char *argv[])
 {
     IJK_INITIALIZE_LOGGING();
-    xx::server_network sn(xx::server_addr(1, 1, 100, 1));
 
     auto addr = asio::ip::make_address("127.0.0.1");
     asio::ip::tcp::endpoint ep(addr, 4000);
 
-    sn.start_server(ep);
-    sn.start_client(xx::server_addr(1, 1, 101, 1), ep);
+    xx::server_addr sa1(1, 1, 100, 1);
+    xx::server_network sn1(sa1);
+    sn1.start_server(ep);
+
+    xx::server_addr sa2(1, 1, 200, 1);
+    xx::server_network sn2(sa2);
+    sn2.start_client(sa1, ep);
 
     while (true) {
-        auto count = sn.poll();
+        auto count = sn1.poll();
+        count += sn2.poll();
         if (count == 0)
             std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
