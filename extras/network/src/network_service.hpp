c78fd7650ad93c755769ff21652583306878c080
lchannng <l.channng@gmail.com>
2019-10-28 16:17:32 +0800

    update.

--- extras/network/src/network_service.hpp
+++ extras/network/src/network_service.hpp
@@ -19,6 +19,7 @@
 #include "protocol.pb.h"
 
 #include <limits>
+#include <map>
 
 namespace xx {
 
@@ -30,33 +31,42 @@ public:
 
     virtual ~network_service_manager() = default;
 
-    bool add_connection(const server_addr& target_addr,
-        server_connection *conn) {
-        return false;
+    bool add_connection(const server_addr &target_addr,
+                        server_connection *conn) {
+        auto res = conns_.emplace(target_addr, conn);
+        return res.second;
     }
 
-    void remove_connection(const server_addr& target_addr,
-        server_connection *conn) {
-
+    void remove_connection(const server_addr &target_addr,
+                           server_connection *conn) {
+        conns_.erase(target_addr);
     }
 
-    server_connection *get_connection(const server_addr& target_addr) {
+    server_connection *get_connection(const server_addr &target_addr) {
         return nullptr;
+        auto iter = conns_.find(target_addr);
+        if (iter == conns_.end()) {
+            return nullptr;
+        }
+        return iter->second;
     }
 
     const server_addr &svr_addr() { return my_svr_addr_; }
 
 protected:
     server_addr my_svr_addr_;
+    std::map<server_addr, server_connection *> conns_;
 };
 
 class server_connection final : public ijk::base_connection<server_connection> {
     using head_type = uint16_t;
+
 public:
     using ijk::base_connection<server_connection>::base_connection;
     ~server_connection() = default;
 
-    void run_on_client_side(network_service_manager &manager, const server_addr &target_addr) {
+    void run_on_client_side(network_service_manager &manager,
+                            const server_addr &target_addr) {
         Expects(!target_addr.is_null());
         manager_ = &manager;
         message_loop(shared_from_this());
@@ -81,8 +91,7 @@ public:
     const server_addr &target_addr() { return target_addr_; }
 
 private:
-    static bool encode(const SNMessage &msg,
-                       std::string &buf) {
+    static bool encode(const SNMessage &msg, std::string &buf) {
         auto len = msg.ByteSizeLong();
         if (len > std::numeric_limits<head_type>::max()) {
             LOG_WARN("invalid payload len {}", len);
@@ -113,7 +122,8 @@ private:
                 SNMessage msg;
                 msg.ParseFromArray(buf_.data(), static_cast<int>(buf_.size()));
                 process(msg);
-            }).finally([this, self](auto e){
+            })
+            .finally([this, self](auto e) {
                 if (e.has_value()) {
                     message_loop(self);
                 } else {
@@ -130,7 +140,7 @@ private:
         send_message(msg);
     }
 
-    inline void send_message(const SNMessage& msg) {
+    inline void send_message(const SNMessage &msg) {
         std::string buf;
         if (encode(msg, buf)) {
             base_connection::send(std::move(buf));
@@ -147,7 +157,7 @@ private:
                     LOG_ERROR("invalid handshake message");
                     close();
                     return;
-                } 
+                }
 
                 auto addr = server_addr(msg.handshake().svr_id());
 
@@ -180,9 +190,8 @@ private:
                     close();
                     return;
                 }
-            
-            }
-            break;
+
+            } break;
             default:
                 break;
         }
