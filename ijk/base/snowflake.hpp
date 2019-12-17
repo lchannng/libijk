a20f69c244c874d2b32bf40ad969c7c4b4d1a43d
lchannng <l.channng@gmail.com>
2019-12-17 11:53:57 +0800

    snowflake.

--- ijk/base/snowflake.hpp
+++ ijk/base/snowflake.hpp
@@ -1,5 +1,5 @@
 /*
- * File Name: id_generator.h
+ * File Name: snowflake.hpp
  * Author: Stan.Lch
  * Mail: fn.stanc@gmail.com
  * Created Time: 2018/08/25 16:28:02
@@ -13,22 +13,34 @@
 #include <exception>
 
 namespace ijk {
-class id_generator {
-    // Id = time(41bits)+ machine(10bits) + sequence(12bits)
+
+// Id = time((63- m -n) bits)+ machine(m bits) + sequence(n bits)
+template<int MachineBits = 10, int SequenceBits = 12>
+class snowflake {
 public:
-    id_generator(int machine_id) {
-        if (machine_id > kMaxMachineId)
+    snowflake() = default;
+
+    ~snowflake() = default;
+
+    snowflake(const snowflake &) = delete;
+
+    snowflake &operator=(const snowflake &) = delete;
+
+
+    void init(int machine_id) {
+        if (machine_id < 0 || machine_id > kMaxMachineId)
             throw std::runtime_error("bad machine id");
         machine_id_ = machine_id;
     }
 
     int64_t next_id() {
-        int64_t now = time_gen();
+        int64_t now = now_ms();
 
         if (now < last_timestamp_) {
             // clock is moving backwards
-            last_timestamp_ = now;
-            sequence_ = 0;
+            // last_timestamp_ = now;
+            // sequence_ = 0;
+            throw std::runtime_error("clock is moving backwards");
         }
 
         if (last_timestamp_ == now) {
@@ -53,7 +65,7 @@ public:
     }
 
 private:
-    int64_t time_gen() {
+    int64_t now_ms() {
         using namespace std::chrono;
         auto duration = system_clock::now().time_since_epoch();
         auto ms = duration_cast<milliseconds>(duration).count();
@@ -63,30 +75,30 @@ private:
     int64_t til_next_millis(int64_t now) {
         int64_t t;
         do {
-            t = time_gen();
+            t = now_ms();
         } while (t <= now);
         return t;
     }
 
 private:
-    static const int64_t kEpoch{1288834974657};
+    static constexpr int64_t kEpoch{1576497960000};
 
-    static const int kMachineBits{10};
-    static const int kSequenceBits{12};
-    static const int kTimeBits{41};
+    static constexpr int64_t kMachineBits{MachineBits};
+    static constexpr int64_t kSequenceBits{SequenceBits};
+    static constexpr int64_t kTimeBits{63 - kMachineBits - kSequenceBits};
 
-    static const int kMachineShift{kSequenceBits};
-    static const int kTimeShift{kMachineBits + kSequenceBits};
+    static constexpr int64_t kMachineShift{kSequenceBits};
+    static constexpr int64_t kTimeShift{kMachineBits + kSequenceBits};
 
-    static const int kSequenceMask{-1 ^ (-1 << kSequenceBits)};
-    static const int kMachineMask{-1 ^ (-1 << kMachineBits)};
-    static const int64_t kTimeMask{(-1LL ^ (-1LL << kTimeBits))};
+    static constexpr int64_t kSequenceMask{-1 ^ (-1 << kSequenceBits)};
+    static constexpr int64_t kMachineMask{-1 ^ (-1 << kMachineBits)};
+    static constexpr int64_t kTimeMask{(-1LL ^ (-1LL << kTimeBits))};
 
-    static const int kMaxMachineId{-1 ^ (-1 << kMachineBits)};
+    static constexpr int kMaxMachineId{-1 ^ (-1 << kMachineBits)};
 
     int64_t last_timestamp_{0};
-    int machine_id_{0};
-    int sequence_{0};
+    int64_t machine_id_{0};
+    int64_t sequence_{0};
 };
 }  // namespace ijk
 #endif
